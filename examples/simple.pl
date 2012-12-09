3d661265144e316bd9ce16f3542fbea57248830a
Olivier Mengué <dolmen@cpan.org>
2012-12-09 22:02:30 +0100

    Fix the demo

--- examples/simple.pl
+++ examples/simple.pl
@@ -20,3 +20,19 @@ use lib "$FindBin::Bin/../lib";
 
 use Test::More tests => 1;
 use Test::DiagRef;
+use Scalar::Util 'weaken';
+
+
+note 'This is a demo of Test::DiagRef. The test is expected to fail.';
+
+my $obj = MyClass->new;
+
+# $ref is a weak reference to the object
+weaken(my $ref = $obj);
+
+# If MyClass was ok, this would destroy the object, and so $ref would become
+# undef.
+undef $obj;
+
+# This test is expected to fail: this is a demo of Test::DiagRef!
+is($ref, undef, 'no leak') or diag_ref($ref);
